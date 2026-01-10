1b6644a74dbcf4acc8587bd40f059c6758dfb9d4
shayz86 <pembang001@gmail.com>
2026-01-11 01:17:45 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,5 @@
-const CACHE_NAME = "game-asah-otak-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./game-color/"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", () => {
+  console.log("Service Worker installed");
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", () => {});
